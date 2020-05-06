34eaeac3d04cb89c098abc884147fabc2be3d08c
kawtarsidk <kawtar.sidk@gmail.com>
2020-05-06 00:32:21 +0000

    firstcommit

---
+++ src/main/java/ma/pfa/gestion_evenement/GestionEvenementApplication.java
@@ -0,0 +1,13 @@
+package ma.pfa.gestion_evenement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionEvenementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionEvenementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ma/pfa/gestion_evenement/GestionEvenementApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.pfa.gestion_evenement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionEvenementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
