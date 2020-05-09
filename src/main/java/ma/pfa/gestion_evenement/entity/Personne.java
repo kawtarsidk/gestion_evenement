0e7fa1a10674936c53b18b3456cdcf3cc38a84ec
kawtarsidk <kawtar.sidk@gmail.com>
2020-05-09 19:07:35 +0000

    modif Personne

--- src/main/java/ma/pfa/gestion_evenement/entity/Personne.java
+++ src/main/java/ma/pfa/gestion_evenement/entity/Personne.java
@@ -1,7 +1,14 @@
 package ma.pfa.gestion_evenement.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
 public class Personne {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String nom;
     private String prenom;
