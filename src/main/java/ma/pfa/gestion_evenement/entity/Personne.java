4daad10a5b37244f193bae0801a661ebb26ad624
kaoutar karchaou <kawtarlulu.kl@gmail.com>
2020-05-09 01:17:29 +0100

    test dans la branche personne , ajout d'attributs

--- src/main/java/ma/pfa/gestion_evenement/entity/Personne.java
+++ src/main/java/ma/pfa/gestion_evenement/entity/Personne.java
@@ -1,5 +1,11 @@
 package ma.pfa.gestion_evenement.entity;
 
 public class Personne {
+    private int id;
+    private String nom;
+    private String prenom;
+    private String mail;
+    private String telephone;
     
 }
+
