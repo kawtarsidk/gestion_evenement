aeacb06153debd0b42685fff4df7f35605517254
kaoutar karchaou <kawtarlulu.kl@gmail.com>
2020-05-09 19:58:00 +0100

    creation de l'entity organisateur

---
+++ src/main/java/ma/pfa/gestion_evenement/entity/Organisateur.java
@@ -0,0 +1,7 @@
+package ma.pfa.gestion_evenement.entity;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Organisateur {
+}
--- src/main/java/ma/pfa/gestion_evenement/entity/Personne.java
+++ src/main/java/ma/pfa/gestion_evenement/entity/Personne.java
@@ -1,5 +1,6 @@
 package ma.pfa.gestion_evenement.entity;
 
+
 public class Personne {
     private int id;
     private String nom;
