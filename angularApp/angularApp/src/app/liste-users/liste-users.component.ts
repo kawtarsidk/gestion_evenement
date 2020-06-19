05eb536f04f5d5614bf3711703d84f8e170fe272
kaoutar karchaou <kawtarlulu.kl@gmail.com>
2020-06-19 22:08:52 +0100

    reglage de l'affichage

--- angularApp/angularApp/src/app/liste-users/liste-users.component.ts
+++ angularApp/angularApp/src/app/liste-users/liste-users.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 
@@ -8,33 +9,37 @@ export interface ListeUsers {
   email: string;
   role: string;
 }
-const ELEMENT_DATA: ListeUsers[] = [
-  {id: 1, nom: 'Ahmed Alami', email: 'alami@gmail.com', role: 'Organisateur'},
- /* {id: 2, nom: 'Leila Chaouki', email: 'Chaouki@gmail.com', role: 'Directeur'},
-  {id: 3, nom: 'Driss Alaoui', email: 'Alaoui@gmail.com', role: 'RespLogistique'},*/
 
+const ELEMENT_DATA: ListeUsers[] = [
+  {id: 1, nom: 'Alami Ahmad', email: 'ahmad@gmail.com', role: 'Directeur'},
+  {id: 2, nom: 'Alaoui Ali', email: 'Ali@gmail.com', role: 'Organisateur'},
+  {id: 3, nom: 'Amina slimani', email: 'Amina@gmail.com', role: 'Responsable'},
 ];
+
 @Component({
-  selector: 'app-liste-users',
+  selector: 'app-evenement',
   templateUrl: './liste-users.component.html',
   styleUrls: ['./liste-users.component.scss']
 })
 export class ListeUsersComponent implements OnInit {
+
   displayedColumns: string[] = ['id', 'nom', 'email', 'role', 'action'];
-  dataSource =ELEMENT_DATA ;
-  //dataSource: MatTableDataSource<ListeEvenement>; Uncomment when u bring the date from the back
+  dataSource = ELEMENT_DATA;
+  //dataSource: MatTableDataSource<ListeUsers>; Uncomment when u bring the date from the back
 
   @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
   @ViewChild(MatSort, {static: true}) sort: MatSort;
-  constructor() { }
 
-  ngOnInit(): void {
-     /*
-    this.dataSource.paginator = this.paginator;
-    this.dataSource.sort = this.sort; */
+  constructor() { 
   }
 
-  
+  ngOnInit(): void {
+    /* UNCOMMENT WHEN U BRING DATA FROM BACK
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort; */
+
+  }
+
  /*  applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
