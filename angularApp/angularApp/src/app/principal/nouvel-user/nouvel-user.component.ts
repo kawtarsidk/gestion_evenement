670e847f046960f5a12457a579b7def39fc33c7c
kaoutar-karchaou <64861930+kaoutar-karchaou@users.noreply.github.com>
2020-06-25 13:51:55 +0200

    fix merge

--- angularApp/angularApp/src/app/principal/nouvel-user/nouvel-user.component.ts
+++ angularApp/angularApp/src/app/principal/nouvel-user/nouvel-user.component.ts
@@ -1,5 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators} from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import {Compte} from "../../login/login.component";
+
+export interface personnes {
+  id: number;
+  cin: string;
+  nom : string;
+  prenom : string;
+  mail : string;
+  telephone : string;
+  compte : Compte;
+}
 
 @Component({
   selector: 'app-nouvel-user',
@@ -8,14 +20,32 @@ import { FormGroup, FormBuilder, Validators} from '@angular/forms';
 })
 export class NouvelUserComponent implements OnInit {
   userForm: FormGroup;
+  error: string | undefined;
+  success : string;
+  isPassed : boolean = false;
+  id : number;
+  compte : Compte;
 
-  constructor(private formBuilder: FormBuilder) { this.createForm();}
+  constructor(private formBuilder: FormBuilder,private httpClient : HttpClient) { this.createForm();}
 
   ngOnInit(): void {
   }
 
   newUser(){
     console.log("userForm", this.userForm.value);
+    const user = this.httpClient.post('http://localhost:8080/Personnes/save',this.userForm.value);
+    user.subscribe(
+      (user: personnes) => {
+        console.log("user",user);
+        this.isPassed = true;
+        this.success = "User crée avec succès";
+      },
+      error => {
+        console.log("error",error);
+        this.isPassed = false;
+        this.error = error.error.message;
+      }
+    );
   }
 
   private createForm() {
