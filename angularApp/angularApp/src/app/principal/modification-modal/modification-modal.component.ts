dcfa3e147105f218f05e9c9cb1874b526e1e66e4
kaoutar karchaou <kawtarlulu.kl@gmail.com>
2020-06-26 16:00:11 +0100

    liste evenements

--- angularApp/angularApp/src/app/principal/modification-modal/modification-modal.component.ts
+++ angularApp/angularApp/src/app/principal/modification-modal/modification-modal.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import {ListeUsers } from '../liste-users/liste-users.component';
 import { HttpClient } from '@angular/common/http';
+import {ListeEvenement} from "../liste-evenement/liste-evenement.component";
 
 @Component({
   selector: 'app-modification-modal',
@@ -12,7 +13,9 @@ export class ModificationModalComponent {
 
   constructor(
     public dialogRef: MatDialogRef<ModificationModalComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: ListeUsers, private httpClient : HttpClient) {}
+    @Inject(MAT_DIALOG_DATA) public data: ListeUsers, private httpClient : HttpClient,
+    @Inject(MAT_DIALOG_DATA) public data1: ListeEvenement, private httpClien :HttpClient
+    ) {}
 
   onNoClick(): void {
     this.dialogRef.close();
--- angularApp/angularApp/src/app/principal/suppression-modal/suppression-modal.component.ts
+++ angularApp/angularApp/src/app/principal/suppression-modal/suppression-modal.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { ListeUsers } from '../liste-users/liste-users.component';
 import { HttpClient } from '@angular/common/http';
+import {ListeEvenement} from "../liste-evenement/liste-evenement.component";
 
 @Component({
   selector: 'app-suppression-modal',
@@ -12,7 +13,8 @@ export class SuppressionModalComponent {
 
   constructor(
     public dialogRef: MatDialogRef<SuppressionModalComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: ListeUsers, private httpClient: HttpClient) { }
+    @Inject(MAT_DIALOG_DATA) public data: ListeUsers, private httpClient: HttpClient,
+    @Inject(MAT_DIALOG_DATA) public data1: ListeEvenement, private httpClien: HttpClient) { }
 
   onNoClick(): void {
     this.dialogRef.close();
